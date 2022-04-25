49dedf1c246d5f2556f9cdbe9b57fbce1f1806cf
GeneralWolf <49901138+GeneralWolf@users.noreply.github.com>
2022-04-25 12:21:41 +0100

    added add_produto to the right file

--- python/app/main.py
+++ python/app/main.py
@@ -1,5 +1,5 @@
-import psycopg2, time
-
+import psycopg2, time, logging
+import flask
 from flask import jsonify, request, Flask
 
 app = Flask(__name__)
@@ -243,6 +243,130 @@ def autenticaUtilizador():
             conn.close()
 
     return jsonify(content)
+
+#TODO cant fucking use produto if inserting another table, can I?
+#TODO wait no i kinda can i believe
+#TODO still not sure if the endpoint should be produto
+@app.route('/produto/', methods=['POST'])
+def add_produto():
+    logger.info('POST /produto')
+    payload = flask.request.get_json()
+
+    conn = db_connection()
+    cur = conn.cursor()
+
+    logger.debug(f'POST /produto - payload: {payload}')
+
+    # do not forget to validate every argument, e.g.,:
+    if 'produto_id' not in payload:
+        response = {'status': StatusCodes['api_error'], 'results': 'produto_id value not in payload'}
+        return flask.jsonify(response)
+
+    #TODO check if better way to do this
+    #TODO update description and price on table versao
+    if(payload['tipo'] == 'computador'):
+        comp_create_stmt = """INSERT INTO computador
+                    (processador, sistema_operativo, armazenamento, camara, produto_id, produto_marca, produto_stock, produto_vendedor)
+                   VALUES
+                    (%s, %s, %s, %s, %d, %s, %d, %d)"""
+        values_comp = (payload['processador'], payload['sistema_operativo'], payload['armazenamento'], payload['camara'], payload['produto_id'], payload['produto_marca'], payload['produto_stock'], payload['produto_vendedor'])
+
+        #TODO trying to also add the versao table part
+        #TODO also why is the version product id still have smartphone in the name???
+        version_create_stmt = """INSERT INTO versao
+                            (descricao, preco, data_alteracao, smartphone_produto_id)
+                            VALUES
+                            (%s, %f, TIMESTAMP, %d)
+                                """
+        values_versao = (payload['descricao'], payload['preco'], payload['produto_id'])
+
+        try:
+            cur.execute(comp_create_stmt, values_comp)
+            cur.execute(version_create_stmt, values_versao)
+
+            # commit the transaction
+            conn.commit()
+            response = {'status': StatusCodes['success'], 'results': f'Inserted produto (computador) {payload["id_produto"]}'}
+
+        except (Exception, psycopg2.DatabaseError) as error:
+            logger.error(f'POST /produto - error: {error}')
+            response = {'status': StatusCodes['internal_error'], 'errors': str(error)}
+
+            # an error occurred, rollback
+            conn.rollback()
+
+        finally:
+            if conn is not None:
+                conn.close()
+
+    elif(payload['tipo'] == 'televisor'):
+        tel_create_stmt = """INSERT INTO televisor
+                    (comprimento, largura, peso, resolucao, produto_id, produto_marca, produto_stock, produto_vendedor)
+                   VALUES
+                    (%d, %d, %d, %s, %d, %s, %d, %d)"""
+        values_tel = (payload['comprimento'], payload['largura'], payload['peso'], payload['resolucao'], payload['produto_id'], payload['produto_marca'], payload['produto_stock'], payload['produto_vendedor'])
+
+        version_create_stmt = """INSERT INTO versao
+                                    (descricao, preco, data_alteracao, smartphone_produto_id)
+                                    VALUES
+                                    (%s, %f, TIMESTAMP, %d)
+                                        """
+        values_versao = (payload['descricao'], payload['preco'], payload['produto_id'])
+
+        try:
+            cur.execute(tel_create_stmt, values_tel)
+            cur.execute(version_create_stmt, values_versao)
+
+            # commit the transaction
+            conn.commit()
+            response = {'status': StatusCodes['success'], 'results': f'Inserted produto (televisor) {payload["id_produto"]}'}
+
+        except (Exception, psycopg2.DatabaseError) as error:
+            logger.error(f'POST /produto - error: {error}')
+            response = {'status': StatusCodes['internal_error'], 'errors': str(error)}
+
+            # an error occurred, rollback
+            conn.rollback()
+
+        finally:
+            if conn is not None:
+                conn.close()
+
+    elif (payload['tipo'] == 'smartphone'):
+        phone_create_stmt = """INSERT INTO smartphone
+                        (modelo, cor, produto_id, produto_marca, produto_stock, produto_vendedor)
+                       VALUES
+                        (%s, %s, %d, %s, %d, %d)"""
+        values_phone = (payload['modelo'], payload['cor'], payload['produto_id'], payload['produto_marca'], payload['produto_stock'], payload['produto_vendedor'])
+
+        version_create_stmt = """INSERT INTO versao
+                                    (descricao, preco, data_alteracao, smartphone_produto_id)
+                                    VALUES
+                                    (%s, %f, TIMESTAMP, %d)
+                                        """
+        values_versao = (payload['descricao'], payload['preco'], payload['produto_id'])
+
+        try:
+            cur.execute(phone_create_stmt, values_phone)
+            cur.execute(version_create_stmt, values_versao)
+
+            # commit the transaction
+            conn.commit()
+            response = {'status': StatusCodes['success'], 'results': f'Inserted produto (smartphone) {payload["id_produto"]}'}
+
+        except (Exception, psycopg2.DatabaseError) as error:
+            logger.error(f'POST /produto - error: {error}')
+            response = {'status': StatusCodes['internal_error'], 'errors': str(error)}
+
+            # an error occurred, rollback
+            conn.rollback()
+
+        finally:
+            if conn is not None:
+                conn.close()
+
+
+    return flask.jsonify(response)
 
 
 #MAIN
